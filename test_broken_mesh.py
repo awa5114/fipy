fc1ae671c72e57051e618da713e110d0e84449b9
aboufira <amine.aboufirass@gmail.com>
2020-02-23 14:52:37 +0100

    modifying test_broken_mesh.py to include resetting of cell type in underlying tvtk object. It turns out that this causes mesh to appear in plotter

--- test_broken_mesh.py
+++ test_broken_mesh.py
@@ -1,13 +1,13 @@
-from fipy import CylindricalGrid2D, Grid2D
+from fipy import Grid2D
 import pyvista
 
-
+# Create Mesh
 mesh = Grid2D(dx = 1.0, dy = 1.0, nx = 2, ny = 2)
+ug = mesh.VTKCellDataSet
+#ug.set_cells([7,7,7,7], ug.cell_locations_array.to_array(), ug.get_cells()) # overwrite to
+ugrid= pyvista.UnstructuredGrid(ug._vtk_obj)
 
-ugrid= pyvista.UnstructuredGrid(mesh.VTKCellDataSet._vtk_obj)
-ugrid.save('broken_mesh.vtk', binary=False)
-
-
+# Plot Mesh
 plotter = pyvista.Plotter()
 plotter.set_background('white')
 plotter.add_mesh(ugrid, style='wireframe', color='black')
